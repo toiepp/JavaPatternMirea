61720ed018c7acf41ebf60d50c9cecd4de8dfff4
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2022-02-10 17:08:08 +0300

    Задание 1 сделано

--- src/me/mikholsky/task1/Main.java
+++ src/me/mikholsky/task1/Main.java
@@ -1,4 +1,17 @@
 package me.mikholsky.task1;
 
+import java.math.BigInteger;
+import java.util.function.BiFunction;
+
+// Вариант 4
+
 public class Main {
+	public static void main(String[] args) {
+		BiFunction<Integer, Integer, Integer> nod = (n1, n2) ->
+				new BigInteger(String.valueOf(n1))
+						.gcd(new BigInteger(String.valueOf(n2)))
+						.intValue();
+
+		System.out.println(nod.apply(11, 33));
+	}
 }
