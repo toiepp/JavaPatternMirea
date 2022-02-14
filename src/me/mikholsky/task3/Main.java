889e433c9e5bc84c017ad7e4fcf963b4bc6e779e
Mikholsky Ivan <ivanmikhmain@gmail.com>
2022-02-14 13:29:21 +0300

    Написатл правильный тест под SyncSet

--- src/me/mikholsky/task3/Main.java
+++ src/me/mikholsky/task3/Main.java
@@ -2,71 +2,45 @@ package me.mikholsky.task3;
 
 // Вариант 4
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class Main {
-	private static void task1() {
-		// Пункт 1
+	private static boolean keepsOrder(Set<Integer> set) {
+		Integer[] arr = set.toArray(new Integer[0]);
+		for (int i = 0; i < arr.length - 1; ++i) {
+			if (arr[i] > arr[i + 1]) return false;
+		}
+		return true;
+	}
 
+	private static void task1() throws InterruptedException {
 //		Set<Integer> testSet = new SyncSet<>();
 		Set<Integer> testSet = new HashSet<>();
 
-		Thread t1 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
-				testSet.add(i * 10);
-			}
-		});
+		Thread t1 = new Thread(() -> IntStream.range(0, 500).forEach(testSet::add));
 
-		Thread t2 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
-				testSet.remove(i * 10);
-			}
-		});
+		Thread t2 = new Thread(() -> IntStream.range(500, 1000).forEach(testSet::add));
 
 		t1.start();
 		t2.start();
 
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		t1.join();
+		t2.join();
 
-		System.out.println(testSet.size());
-	}
-
-	private static void task2() {
-//		Map<Integer, Integer> testMap = new HashMap<>();
-		Map<Integer, Integer> testMap = Collections.synchronizedMap(new HashMap<>());
-//		Map<Integer, Integer> testMap = new LockMap<>();
-
-		var t1 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
-				testMap.put(i, i * 10);
-			}
-		});
-
-		var t2 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
-				testMap.remove(i, i * 10);
-			}
-		});
-
-		t1.start();
-		t2.start();
-
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println(testMap.size());
+		System.out.println(keepsOrder(testSet));
 	}
 
 	public static void main(String[] args) {
-		task2();
+		IntStream.range(0, 1000)
+				.forEach(el -> {
+					System.out.print(el + ": ");
+					try {
+						task1();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				});
 	}
 }
