5dac8dc7fbe5c379c70c246cdda8680701387b65
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2022-02-14 12:13:01 +0300

    Сохраняю результат в удаленный репозиторий

--- src/me/mikholsky/task3/LockMap.java
+++ src/me/mikholsky/task3/LockMap.java
@@ -25,61 +25,115 @@ public class LockMap<K, V> implements Map<K, V> {
 
 	@Override
 	public int size() {
-		return 0;
+		lock.lock();
+		try {
+			return map.size();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		lock.lock();
+		try {
+			return map.isEmpty();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public boolean containsKey(Object key) {
-		return false;
+		lock.lock();
+		try {
+			return map.containsKey(key);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public boolean containsValue(Object value) {
-		return false;
+		lock.lock();
+		try {
+			return map.containsValue(value);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public V get(Object key) {
-		return null;
+		lock.lock();
+		try {
+			return map.get(key);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public V put(K key, V value) {
-		return null;
+		lock.lock();
+		try {
+			return map.put(key, value);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public V remove(Object key) {
-		return null;
+		lock.lock();
+		try {
+			return map.remove(key);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public void putAll(Map<? extends K, ? extends V> m) {
-
+		lock.lock();
+		map.putAll(m);
+		lock.unlock();
 	}
 
 	@Override
 	public void clear() {
-
+		lock.lock();
+		map.clear();
+		lock.unlock();
 	}
 
 	@Override
 	public Set<K> keySet() {
-		return null;
+		lock.lock();
+		try {
+			return map.keySet();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public Collection<V> values() {
-		return null;
+		lock.lock();
+		try {
+			return map.values();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
 	public Set<Entry<K, V>> entrySet() {
-		return null;
+		lock.lock();
+		try {
+			return map.entrySet();
+		} finally {
+			lock.unlock();
+		}
 	}
 }
--- src/me/mikholsky/task3/Main.java
+++ src/me/mikholsky/task3/Main.java
@@ -2,9 +2,7 @@ package me.mikholsky.task3;
 
 // Вариант 4
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 	private static void task1() {
@@ -38,7 +36,37 @@ public class Main {
 		System.out.println(testSet.size());
 	}
 
-	public static void main(String[] args) {
+	private static void task2() {
+//		Map<Integer, Integer> testMap = new HashMap<>();
+		Map<Integer, Integer> testMap = Collections.synchronizedMap(new HashMap<>());
+//		Map<Integer, Integer> testMap = new LockMap<>();
 
+		var t1 = new Thread(() -> {
+			for (int i = 0; i < 1000; ++i) {
+				testMap.put(i, i * 10);
+			}
+		});
+
+		var t2 = new Thread(() -> {
+			for (int i = 0; i < 1000; ++i) {
+				testMap.remove(i, i * 10);
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(testMap.size());
+	}
+
+	public static void main(String[] args) {
+		task2();
 	}
 }
