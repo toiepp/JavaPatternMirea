7570ca107abd89bfaa39a6928e64b2fcbeb76691
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2022-02-23 17:20:03 +0300

    Задания 11, 12: сохранение прогресса в локальный репозиторий

---
+++ src/me/mikholsky/task11/task11/src/main/java/me/mikholskiy/task11/Task11Application.java
@@ -0,0 +1,14 @@
+package me.mikholskiy.task11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class Task11Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Task11Application.class, args);
+	}
+}
