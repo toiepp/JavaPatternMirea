11d14db7eedbc8c966d3f4d13523d54f4b1fd9c9
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2022-03-03 11:30:24 +0300

    Задание 11 выполнено

---
+++ src/me/mikholsky/task11/task11/src/main/java/me/mikholskiy/task11/Task11Application.java
@@ -0,0 +1,11 @@
+package me.mikholskiy.task11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task11Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Task11Application.class, args);
+	}
+}
---
+++ src/me/mikholsky/task11/task11/src/test/java/me/mikholskiy/task11/Task11ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mikholskiy.task11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
