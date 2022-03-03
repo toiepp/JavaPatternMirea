b7fd903928d8aaf714f46344049907571525e808
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2022-03-03 13:14:43 +0300

    Выполнил задание 1

--- src/me/mikholsky/task10/task10/src/main/java/ApplicationContext.java
+++ src/me/mikholsky/task10/task10/src/main/java/ApplicationContext.java
@@ -4,20 +4,24 @@ import classes.Lamp;
 import classes.Lighter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class ApplicationContext {
 	@Bean
+	@Lazy
 	public Lighter lamp() {
 		return new Lamp();
 	}
 
 	@Bean
+	@Lazy
 	public Lighter flashlight() {
 		return new Flashlight();
 	}
 
 	@Bean
+	@Lazy
 	public Lighter firefly(){
 		return new Firefly();
 	}
